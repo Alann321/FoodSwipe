63688202edf7d659ad72ce255a724a60c1e14098
Enyi Asonye <enyiasonye@gmail.com>
2018-04-23 14:55:40 -0500

    designed out dashboard page, added components for swiping and list of restaurants and styling

--- FoodSwipe/src/app/app.module.ts
+++ FoodSwipe/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RoutingModule } from './routing.module';
+import { ResultsComponent } from './results/results.component';
+import { MainComponent } from './main/main.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    ResultsComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
