afb23a87775d2b5850544527ff51f332418e5207
Enyi Asonye <enyiasonye@gmail.com>
2018-04-30 18:02:04 -0500

    fixed up code in dashboard component and location search now workds

--- FoodSwipe/src/app/dashboard/dashboard.component.ts
+++ FoodSwipe/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,4 @@
 import { GeoLocation } from './../domain/models/geolocation';
-import { EnteredLocation } from './../domain/models/enteredlocation';
 import { ApiRepositoryService } from './../domain/api-repository.service';
 import { Component, OnInit, Input, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
@@ -16,31 +15,35 @@ export class DashboardComponent implements OnInit {
 
 
   @Input()
-  public enteredLocation: EnteredLocation;
-
+  public enteredLocation: string;
+  // otherwise user sees + replacing spaces in text box
+  public passedInLocation: string;
   public newLocation: GeoLocation;
 
   constructor(
-    private apiRepo: ApiRepositoryService
+    private apiRepo: ApiRepositoryService,
+    private router: Router
   ) { }
 
   ngOnInit() {
     // initialize the data inside the location as blank
     this.newLocation = { };
 
-    this.enteredLocation = {
-      entry: ''
-    };
+    this.enteredLocation = '';
   }
 
 
 
   public searchLocation() {
     console.log('Searching Location');
-
+    // replace spaces with plus
+    this.passedInLocation = this.enteredLocation.replace(/\s/g, '+');
     // search the user's location:
-    this.apiRepo.getLocation(this.enteredLocation.entry).subscribe(data => {
-      console.log(data);
+    this.apiRepo.getLocation(this.passedInLocation).subscribe(data => {
+      this.newLocation.latitude = data.results[0].geometry.location.lat; // filler
+      this.newLocation.longitude = data.results[0].geometry.location.lng; // filler
+      this.router.navigateByUrl('/main');
+      console.log(this.newLocation);
     });
 
   }
@@ -51,6 +54,7 @@ export class DashboardComponent implements OnInit {
       this.newLocation.latitude = data.location.lat;
       this.newLocation.longitude = data.location.lng;
       console.log(this.newLocation);
+      this.router.navigateByUrl('/main');
 
     });
 
--- FoodSwipe/src/app/domain/models/enteredlocation.ts
+++
@@ -1,3 +0,0 @@
-export class EnteredLocation {
-  entry: string;
-}
